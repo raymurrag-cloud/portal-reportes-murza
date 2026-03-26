38b6f5bf99bd9d5cfc27cbb2c675c98361b53532
Ray Murra <raymurrag@gmail.com>
2026-03-25 20:39:28 -0600

    Fix: corrige estructura JSX (div extra causaba build error)

--- frontend/src/components/portal/ReportePage.jsx
+++ frontend/src/components/portal/ReportePage.jsx
@@ -112,8 +112,6 @@ export default function ReportePage() {
                 }
               }}
             >{contenido}</ReactMarkdown>
-          </div>
-
           </div>{/* /reporte-contenido */}
 
           {/* Paywall */}
